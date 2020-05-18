71161ada412fff73b9f6e958e866cfb96071b6ee
KapplDw <55997890+KapplDw@users.noreply.github.com>
2020-05-18 14:56:01 -0700

    First part done

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 /*
  * Write your server code in this file.
  *
- * name:
- * email:
+ * name: Dwight Kappl  
+ * email: Kappldw@oregonstate.edu
  */
+
+var http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+var server = http.createServer(function (req, res) {
+    console.log("== Got a request");
+    console.log("  -- HTTP method:", req.method);
+    console.log("  -- Resource:", req.url);
+    console.log("  -- Headers:", req.headers);
+  
+    res.writeHead(200, {
+      "Content-Type": "text/html"
+    });
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<h1>Hello World!</h1>");
+    res.write("<p>You requested this page: " + req.url + "</p>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+});
+
+
+server.listen(PORT, function () {
+    console.log("== Server is listening on port " + PORT);
+});
